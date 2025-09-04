391905b998520a5cc1c6cb8368d585bfeced6612
L. M. Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2025-09-04 14:08:53 +0100

    update protein.py - include fetch_protein_sequences_from_dataframe - remove logging and try-catch in encoding function - rename encode_protein_sequence to protein_sequence_to_integer_encoding - add docstring on top for the module description

--- mmai25_hackathon/io/protein.py
+++ mmai25_hackathon/io/protein.py
@@ -1,14 +1,46 @@
-import logging
+"""
+Provides utility functions for handling protein sequences, including reading from
+dataframes or CSV files and converting sequences to integer-encoded representations.
+"""
+
+from typing import Union
 
 import numpy as np
+import pandas as pd
 
-# Generate character set for protein sequences
+# Generate character set for protein sequences between A-Z (except J)
 CHARPROTSET = [chr(i) for i in range(ord("A"), ord("Z") + 1) if chr(i) != "J"]
-# Zero is used for padding or unknown characters
-CHARPROTSET = {letter: idx + 1 for idx, letter in enumerate(CHARPROTSET)}
+# Zero is used for padding or unknown characters hence increment from one.
+CHARPROTSET = {letter: idx for idx, letter in enumerate(CHARPROTSET, 1)}
 
 
-def encode_protein_sequence(sequence: str, max_length: int = 1200) -> np.ndarray:
+def fetch_protein_sequences_from_dataframe(
+    df: Union[pd.DataFrame, str], prot_seq_col: str, index_col: str = None
+) -> pd.DataFrame:
+    """
+    Fetches protein sequences from a DataFrame or CSV file. Will read the CSV if a path is provided.
+
+    Args:
+        df (Union[pd.DataFrame, str]): DataFrame or path to CSV file.
+        prot_seq_col (str): Column name for protein sequences.
+        index_col (str, optional): Column to set as index. Default: None.
+
+    Returns:
+        pd.DataFrame: A single column DataFrame containing the protein sequences with name `"protein_sequence"`.
+    """
+    if isinstance(df, str):
+        df = pd.read_csv(df)
+
+    if prot_seq_col not in df.columns:
+        raise ValueError(f"Column '{prot_seq_col}' not found in DataFrame.")
+
+    if index_col is not None:
+        df = df.set_index(index_col)
+
+    return df[prot_seq_col].to_frame("protein_sequence")
+
+
+def protein_sequence_to_integer_encoding(sequence: str, max_length: int = 1200) -> np.ndarray:
     """
     Converts a protein sequence into an integer-encoded representation.
 
@@ -23,13 +55,5 @@ def encode_protein_sequence(sequence: str, max_length: int = 1200) -> np.ndarray
     encoded_sequence = np.zeros(max_length, dtype=np.uint64)
     for i, char in enumerate(sequence[:max_length]):
         # If character is not in CHARPROTSET, it will be skipped and assumed to be unknown
-        try:
-            encoded_sequence[i] = CHARPROTSET[char]
-        except KeyError:
-            logging.warning(
-                (
-                    f"Character '{char}' does not exists in sequence category encoding. "
-                    "Will be skipped and treated as padding with index 0."
-                )
-            )
+        encoded_sequence[i] = CHARPROTSET.get(char, 0)
     return encoded_sequence
