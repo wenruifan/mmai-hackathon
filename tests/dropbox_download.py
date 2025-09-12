e8faaf22db96c2eeb21e5cedbbfca776654aba23
L. M. Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2025-09-12 10:06:08 +0100

    add tests/dropbox_download.py

--- tests/dropbox_download.py
+++ tests/dropbox_download.py
@@ -5,11 +5,33 @@ import zipfile
 import dropbox
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download a folder from Dropbox.")
-    parser.add_argument("dropbox_folder", type=str, help="Path to the Dropbox folder to download.")
-    parser.add_argument("local_folder", type=str, help="Local directory to save downloaded files.")
-    parser.add_argument("--access_token", type=str, default=os.getenv("DROPBOX_TOKEN"), help="Dropbox access token.")
-    parser.add_argument("--unzip", action="store_true", help="Unzip the downloaded folder and remove the zip file.")
+    parser = argparse.ArgumentParser(
+        description="Download a folder from Dropbox (by default downloads MMAI25Hackathon dataset)."
+    )
+    parser.add_argument(
+        "--dropbox-folder",
+        type=str,
+        help="Path to the Dropbox folder to download.",
+        default="/MMAI25Hackathon",
+    )
+    parser.add_argument(
+        "--local-folder",
+        type=str,
+        help="Local directory to save downloaded files.",
+        default="./MMAI25Hackathon",
+    )
+    parser.add_argument(
+        "--access-token",
+        type=str,
+        help="Dropbox access token.",
+        default=os.getenv("DROPBOX_TOKEN"),
+    )
+    parser.add_argument(
+        "--unzip",
+        action=argparse.BooleanOptionalAction,
+        help="Unzip the downloaded folder and remove the zip file.",
+        default=True,
+    )
     args = parser.parse_args()
 
     # get data from dropbox (assumes DROPBOX_TOKEN is set in env)
